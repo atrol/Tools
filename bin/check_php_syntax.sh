d22fbe446386b8513467b88ec251dc8613f47a69
Roland Becker <roland@atrol.de>
2012-09-03 15:45:53 +0200

    Using a modern approach, thanks to Damien

--- bin/check_php_syntax.sh
+++ bin/check_php_syntax.sh
@@ -1,23 +1,11 @@
 #!/bin/bash
-temp_script=`mktemp`
-temp_result=`mktemp`
-
-cat << '+++' >> $temp_script
-#/bin/bash
-php -l $1 >/dev/null 2>> $2
-+++
-chmod +x $temp_script
-
-find . -name '*.php' -exec $temp_script {} $temp_result \;
-rm -f $temp_script
-set `wc -c $temp_result`
-num_bytes=$1
-if test $num_bytes -ne 0; then
-  cat $temp_result
-  ret=1
-else
-  ret=0
-fi
-rm -f $temp_result
-
+ret=0
+for f in $(find . -name '*.php')
+do 
+    php -l $f >/dev/null
+    if [ $? -gt 0 ]
+    then
+        ret=1
+    fi
+done
 exit $ret
