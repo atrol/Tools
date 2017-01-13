1e7dad1da0795d08e8f676489d415a97d8b6a788
Roland Becker <roland@atrol.de>
2017-01-13 22:10:28 +0100

    Add shell to add remotes of MantisBT devs

---
+++ bin/add_mantisdev_remotes.sh
@@ -0,0 +1,6 @@
+git remote add atrol https://github.com/atrol/mantisbt.git
+git remote add cproensa https://github.com/cproensa/mantisbt.git
+git remote add dregad https://github.com/dregad/mantisbt.git
+git remote add jllano https://github.com/jllano/mantisbt.git
+git remote add syncguru https://github.com/syncguru/mantisbt.git
+git remote add vboctor https://github.com/vboctor/mantisbt.git
